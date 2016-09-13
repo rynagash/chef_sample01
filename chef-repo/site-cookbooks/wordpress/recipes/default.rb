c43880e9bf41f722f58e72e2352a66b16629838c
rynagash <nagashi.nishi@gmail.com>
2016-09-14 01:19:51 +0900

    update

--- chef-repo/site-cookbooks/wordpress/recipes/default.rb
+++ chef-repo/site-cookbooks/wordpress/recipes/default.rb
@@ -6,3 +6,22 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+# vagrant かそうでないかでuser名を変更
+user = "vagrant"
+wordpress = "wordpress-4.6.1-ja.tar.gz"
+# https://ja.wordpress.org/wordpress-4.6.1-ja.zip
+# https://ja.wordpress.org/wordpress-4.6.1-ja.tar.gz
+
+cookbook_file "wordpress" do
+  source wordpress
+  user user
+  group user
+  path "/home/#{user}/#{wordpress}"
+end
+
+execute "install wordpress" do
+  user user
+  group user
+  command "tar zxvf /home/#{user}/#{wordpress} -C /home/#{user}/"
+  action :run
+end
